8de95c7d96e18d870dd9fb555c301d503ae9fff4
fanquqi <fanquqi@gmail.com>
2017-07-20 10:20:04 +0800

    add ucloud sdk to influxdb

---
+++ python-sdk-v2/config.py
@@ -0,0 +1,7 @@
+#-*- encoding: utf-8 -*-
+#配置公私钥"""
+public_key  = "=="
+private_key = "5e"
+#project_id = "t" # 项目ID 请在Dashbord 上获取
+#project_id = "" # 项目ID 请在Dashbord 上获取
+base_url    = "https://api.ucloud.cn"
